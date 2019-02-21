f8f420181c6905c7c0270d8b5dd178e40a9bd2bf
Youngmin Kim <youngmnk@umich.edu>
2019-02-20 19:39:59 -0500

    initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/youngminkim/Desktop/664_django/django_projects/projym/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
